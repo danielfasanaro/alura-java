bfae61f680e63898e7d750fd74c30d865caf5946
danielfasanaro <danielfasanaro@gmail.com>
2023-06-16 16:58:31 -0300

    feat: temperatureconverter module

---
+++ TemperatureConverter/src/Main.java
@@ -0,0 +1,10 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+  public static void main(String[] args) {
+    int celsiusTemp = 30;
+    int fahrenheitTemp = (int) (celsiusTemp * 1.8) + 32;
+    System.out.println("Temperatura em graus Celsius: " + celsiusTemp);
+    System.out.println("Temperatura em graus Fahrenheit: " + fahrenheitTemp);
+  }
+}
